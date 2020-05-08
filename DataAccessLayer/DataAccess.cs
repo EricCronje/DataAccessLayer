58725ded7140bc9b5cef5e61c465f6c749211324
ericcronje <eric.cronje@gmail.com>
2020-05-08 10:30:18 +0200

    Project added

---
+++ DataAccessLayer/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    /// <summary>
+    /// Version:
+    /// 1.0.0.0
+    /// 
+    /// Author:
+    /// Eric Cronje
+    /// 
+    /// Date:
+    /// 2020-05-08
+    /// 
+    /// Purpose:
+    /// --------
+    /// Separate the data operations from the main program
+    ///
+    /// </summary>
+    public class DataAccess
+    {
+    }
+}
